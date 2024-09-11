8bd678d138580b4c763e36999f2a8ebb565fdb65
kasturi.saimahesh <kasturi.saimahesh@TEKFRIDAY163>
2024-09-11 18:21:21 +0530

    Add project files.

---
+++ NumberGuessingGame/Controllers/GameController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NumberGuessingGame.Models;
+
+public class GameController : Controller
+{
+    private static GameModel gameModel = new GameModel();
+
+    public IActionResult Index()
+    {
+        if (gameModel.SecretNumber == 0)
+        {
+            gameModel.SecretNumber = new Random().Next(1, 101); // Generate a random number between 1 and 100
+        }
+        return View(gameModel);
+    }
+
+    [HttpPost]
+    public IActionResult Guess(int guess)
+    {
+        gameModel.UserGuess = guess;
+
+        if (guess < gameModel.SecretNumber)
+        {
+            gameModel.ResultMessage = "Too low!";
+        }
+        else if (guess > gameModel.SecretNumber)
+        {
+            gameModel.ResultMessage = "Too high!";
+        }
+        else
+        {
+            gameModel.ResultMessage = "Congratulations! You guessed it right!";
+        }
+
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult Reset()
+    {
+        gameModel = new GameModel(); // Reset the game
+        return RedirectToAction("Index");
+    }
+}
+namespace NumberGuessingGame.Controllers
+{
+    public class GameController
+    {
+    }
+}
---
+++ NumberGuessingGame/Models/GameModel.cs
@@ -0,0 +1,10 @@
+﻿namespace NumberGuessingGame.Models
+{
+    public class GameModel
+    {
+        public int SecretNumber { get; set; }
+        public int? UserGuess { get; set; }
+        public string ResultMessage { get; set; }
+    }
+
+}
